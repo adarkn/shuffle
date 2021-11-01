2a18bc4226aca39e6be6d29d6d48ac658eab654e
adarkn <itzxnexus@gmail.com>
2021-11-01 16:53:48 +0100

    Add command handler

--- index.js
+++ index.js
@@ -28,4 +28,34 @@ require('./src/funcs/eventHandler.js')(client);
 logger.info('Shuffle process started, connecting to discord...');
 process.title = `Shuffle Framework | ${freeRam} GB / ${totalRam} GB | Connecting to discord`
 
+
+/* -- Command handler -- */
+
+/* Creating the commands and aliases collection */
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+
+/* Using fs to read the cmd directory and looks for commands */
+
+fs.readdir('./src/cmd', (err, files) =>{
+
+    /* If we find an error, log it */
+    if (err) logger.error(err);
+
+    /* Logging the number of commands located in ./src/cmd */
+    logger.info(`Found a total of ${files.length} commands, loading them...`);
+
+    /* Loading each command on the files array */
+    files.forEach(f => {
+        const props = require(`./src/cmd/${f}`);
+        logger.misc(`Loading command: ${props.help.name}`);
+        client.commands.set(props.help.name, props);
+        props.conf.aliases.forEach(alias => {
+            client.aliases.set(alias, props.help.name);
+        });
+    });
+});
+
+
 client.login(config.token);
