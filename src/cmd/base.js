4892e81b34ee7abd35836c2c85f42da09dd0b90f
adarkn <16885359+adarkn@users.noreply.github.com>
2021-11-01 18:50:25 +0100

    Add base command file

---
+++ src/cmd/base.js
@@ -0,0 +1,21 @@
+exports.run = (client, message) => {
+
+    /* This is the base file for creating more commands. */
+
+};
+
+/* Configuration for the command handler */
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: []
+};
+
+/* Information for the help command */
+
+exports.help = {
+    name: '',
+    description: '',
+    usage: ''
+};
