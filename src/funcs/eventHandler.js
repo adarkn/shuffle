a1402092f4dde1a2d524674e0636ff2d7e93c76e
adarkn <16885359+adarkn@users.noreply.github.com>
2021-11-02 01:06:47 +0100

    Fixes #3 and handles the createguild event

--- src/funcs/eventHandler.js
+++ src/funcs/eventHandler.js
@@ -4,7 +4,8 @@ const logger = require('./logger.js');
 module.exports = (client) => {
     client.on('ready', () => reqEvent('ready')(client, logger));
     client.on('reconnecting', () => reqEvent('reconnecting')(client, logger));
-    client.on('disconnect', () => reqEvent('ready')(client, logger));
-    client.on('message', () => reqEvent('ready')());
+    client.on('disconnect', () => reqEvent('disconnect')(client, logger));
+    client.on('message', () => reqEvent('message')());
+    client.on('guildcreate', () => reqEvent('guildcreate')(guild, logger));
     
 }
